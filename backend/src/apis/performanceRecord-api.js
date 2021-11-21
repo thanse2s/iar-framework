87d610a4e2ccb60539aa43f990154f3a8ef48501
ToHan <tohan0510@gmail.com>
2021-11-21 13:51:25 +0100

    evekation record full

--- backend/src/apis/performanceRecord-api.js
+++ backend/src/apis/performanceRecord-api.js
@@ -1,18 +1,25 @@
 const performanceservice = require('../services/performancerecords-service');
 const {Salesman} = require("../../../frontend/src/app/models/performance");
 
-exports.add = async function (db, performancerecord){
+exports.add = function (req, res){
+    const db = req.app.get('db');
+    performanceservice.get(db,performancereq);
+}
+
+exports.get = function (req, res){
+    const db = req.app.get('db');
+    id=req.params.id
+    id=req.params.year
+    performanceservice.get(db,id,year);
+}
+
+exports.update = function (req, res){
 
 }
 
-exports.get = async function(db,employee_id,year){
-
-}
-
-exports.update = async function(db, performancerecord){
-
-}
-
-exports.delete = async function(db, employee_id, year){
-
+exports.delete = function (req, res){
+    const db = req.app.get('db');
+    id=req.params.id
+    id=req.params.year
+    performanceservice.delete(db,id,year);
 }
