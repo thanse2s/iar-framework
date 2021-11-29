38933495347516c3de45c298aea23109c987d046
Masternuss <busse.l.95@gmail.com>
2021-11-29 15:16:43 +0100

    first draft for performanceRecord-api.js and performancerecords-service.js; minor updates to app.js

--- backend/src/apis/performanceRecord-api.js
+++ backend/src/apis/performanceRecord-api.js
@@ -1,25 +1,44 @@
-const performanceservice = require('../services/performancerecords-service');
-const {Salesman} = require("../models/PerformanceRecord");
+const performanceService = require('../services/performancerecords-service');
+const {performanceRecord} = require("../models/PerformanceRecord");
 
 exports.add = function (req, res){
     const db = req.app.get('db');
-    performanceservice.get(db,performancereq);
+    let id = req.params.id;
+    let year = req.params.year;
+    let social_performance = req.params.social_performance;
+    let orders_evaluation = req.params.orders_evaluation;
+    let performanceRecord = new performanceRecord(year,id,social_performance,orders_evaluation);
+    performanceService.add(db,performanceRecord)
+        .then( _=> res.status(200).send("Performance Record has been created."))
+        .catch( _=> res.status(401).send("Error. Couldn't create Performance Record!"));
 }
 
 exports.get = function (req, res){
     const db = req.app.get('db');
-    id=req.params.id
-    id=req.params.year
-    performanceservice.get(db,id,year);
+    let id=req.params.id
+    let year=req.params.year
+    performanceService.get(db,id,year)
+        .then(performanceRecord => res.status(200).json(performanceRecord))
+        .catch( _=> res.status(401).send(`Error. No Performance Record for id: ${id} and year: ${year} found!`));
 }
 
 exports.update = function (req, res){
-
+    const db = req.app.get('db');
+    let id = req.params.id;
+    let year = req.params.year;
+    let social_performance = req.params.social_performance;
+    let orders_evaluation = req.params.orders_evaluation;
+    let performanceRecord = new performanceRecord(year,id,social_performance,orders_evaluation);
+    performanceService.update(db, performanceRecord)
+        .then( _=> res.status(200).send("Performance Record has been updated!")).
+        catch( _=> res.status(401).send(`Error. No Performance Record could be updated with id: ${id} and year: ${year}`));
 }
 
 exports.delete = function (req, res){
     const db = req.app.get('db');
-    id=req.params.id
-    id=req.params.year
-    performanceservice.delete(db,id,year);
+    let id=req.params.id
+    let year=req.params.year
+    performanceService.delete(db,id,year)
+        .then( _=> res.status(200).send(`Performance Record with id: ${id} and year: ${year} has been deleted!`))
+        .catch( _=> res.status(400).send(`Error. Couldn't delete Perfomance Record with id: ${id} and year: ${year}!`));
 }
