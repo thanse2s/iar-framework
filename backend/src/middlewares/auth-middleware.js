5447a65c0879210d99f6241ae93d5ff431fd0699
Frederick Behringer <frederick.behringer@smail.inf.h-brs.de>
2022-01-26 16:15:35 +0100

    Refactored Authorization Middleware. One Function got lost in the last merge

--- backend/src/middlewares/auth-middleware.js
+++ backend/src/middlewares/auth-middleware.js
@@ -1,17 +1,29 @@
 /**
- * this express middleware checks if a user is authenticated or even has admin permissions;
+ * this express middleware checks if a user is authenticated;
  * otherwise the request gets intercepted and status 401 is returned
- * @param {boolean} beAdmin if true, user needs to be admin
  * @return {(function(*, *, *): void)|*}
  */
-exports.checkAuthorization = (beAdmin) => {
-    return (req, res, next) => {
-        if(req.session.authenticated){ //check if session was marked as authenticated
-            if(!beAdmin || req.session.user.isAdmin){ //check if admin-requirement is met
-                next(); //proceed with next middleware or handler
+exports.checkAuthorizationForAllRoles = (req, res, next) => {
+    if(req.session.authenticated){ //check if session was marked as authenticated
+        next(); //proceed with next middleware or handler
+        return;
+    }
+    res.status(401).send(); //intercept request
+}
+/**
+ * this express middleware checks if a user is authenticated or even has admin permissions;
+ * otherwise the request gets intercepted and status 401 is returned
+ * @param {string} requiredRole
+ * @return {(function(*, *, *): void)|*}
+ */
+exports.checkAuthorizationByRole = (requiredRole) =>{
+    return(req,res,next) =>{
+        if(req.session.authenticated){
+            if(requiredRole === req.session.user.role || req.session.user.isAdmin){
+                next();
                 return;
             }
         }
-        res.status(401).send(); //intercept request
+        res.status(401).send();
     }
 }
--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {checkAuthorization, haveRole} = require('../middlewares/auth-middleware');
+const {checkAuthorizationForAllRoles, checkAuthorizationByRole} = require('../middlewares/auth-middleware');
 
 /*
     In this file is the routing for the REST-endpoints under /api managed
@@ -8,40 +8,40 @@ const {checkAuthorization, haveRole} = require('../middlewares/auth-middleware')
 
 const authApi = require('../apis/auth-api'); //api-endpoints are loaded from separate files
 router.post('/login', authApi.login); //the function decides which request type should be accepted
-router.delete('/login',checkAuthorization(),authApi.logout); //middlewares can be defined in parameters
+router.delete('/login',checkAuthorizationForAllRoles, authApi.logout); //middlewares can be defined in parameters
 router.get('/login', authApi.isLoggedIn); //the function, which handles requests is specified as the last parameter
 
 const userApi = require('../apis/user-api');
-router.get('/user', checkAuthorization(), userApi.getSelf);
+router.get('/user', checkAuthorizationForAllRoles, userApi.getSelf);
 
 
 const salesmanApi = require('../apis/salesman-api');
-router.get('/salesman/:id',haveRole("manager"), salesmanApi.getOne);
-router.post('/salesman',haveRole("manager"), salesmanApi.addSalesMan);
-router.delete('/salesman/:id',haveRole("manager"), salesmanApi.deleteSalesMan);
-router.get('/salesman',haveRole("manager"), salesmanApi.getAll);
+router.get('/salesman/:id', checkAuthorizationByRole("manager"), salesmanApi.getOne);
+router.post('/salesman', checkAuthorizationByRole("manager"), salesmanApi.addSalesMan);
+router.delete('/salesman/:id', checkAuthorizationByRole("manager"), salesmanApi.deleteSalesMan);
+router.get('/salesman', checkAuthorizationByRole("manager"), salesmanApi.getAll);
 
 // OrangeHRM
 const bonusSalaryApi = require('../apis/bonussalary-api');
-router.get('/bonussalary/:id',checkAuthorization(), bonusSalaryApi.get)
-router.post('/bonussalary/:id',haveRole("HR"), bonusSalaryApi.post)
-router.delete('/bonussalary/:id',haveRole("HR"), bonusSalaryApi.delete)
+router.get('/bonussalary/:id', checkAuthorizationForAllRoles, bonusSalaryApi.get)
+router.post('/bonussalary/:id', checkAuthorizationByRole("HR"), bonusSalaryApi.post)
+router.delete('/bonussalary/:id', checkAuthorizationByRole("HR"), bonusSalaryApi.delete)
 
 const performanceApi = require('../apis/performanceRecord-api');
 const {postBonusSalary, commit, correctBonusInBody} = require('../middlewares/performance-middleware')
-router.get('/performance/uncommitted',checkAuthorization, performanceApi.getCommitted);
-router.get('/performance/:id',checkAuthorization, performanceApi.get);
-router.get('/performance',checkAuthorization, performanceApi.get);
-router.post('/performance/commit/:id',haveRole("HR"), postBonusSalary, commit);
-router.post('/performance/calculatebonus/:id',haveRole("manager"), correctBonusInBody, performanceApi.sendBack);
-router.post('/performance/:id',haveRole("manager"), correctBonusInBody, performanceApi.update);
-router.post('/performance',haveRole("manager"), correctBonusInBody, performanceApi.add);
-router.delete('/performance/:id',haveRole("manager"), performanceApi.delete);
+router.get('/performance/uncommitted', checkAuthorizationForAllRoles, performanceApi.getCommitted);
+router.get('/performance/:id', checkAuthorizationForAllRoles, performanceApi.get);
+router.get('/performance', checkAuthorizationForAllRoles, performanceApi.get);
+router.post('/performance/commit/:id',checkAuthorizationByRole("HR"), postBonusSalary, commit);
+router.post('/performance/calculatebonus/:id', checkAuthorizationByRole("manager"), correctBonusInBody, performanceApi.sendBack);
+router.post('/performance/:id', checkAuthorizationByRole("manager"), correctBonusInBody, performanceApi.update);
+router.post('/performance', checkAuthorizationByRole("manager"), correctBonusInBody, performanceApi.add);
+router.delete('/performance/:id', checkAuthorizationByRole("manager"), performanceApi.delete);
 module.exports = router;
 
-router.post('/performance/:id',haveRole("manager"), performanceApi.update);
-router.post('/performance',haveRole("manager"), performanceApi.add);
-router.delete('/performance/:id',haveRole("manager"), performanceApi.delete);
+router.post('/performance/:id', checkAuthorizationByRole("manager"), performanceApi.update);
+router.post('/performance', checkAuthorizationByRole("manager"), performanceApi.add);
+router.delete('/performance/:id', checkAuthorizationByRole("manager"), performanceApi.delete);
 module.exports = router;
 
 const openCRXApi = require('../apis/opencrx-api');
